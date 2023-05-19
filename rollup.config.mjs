dcc428876420ffda5e0709df2c124cdaec6bcc28
风度万人迷 <805391114@qq.com>
2023-05-19 11:23:27 +0800

    [release] 0.0.7

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,19 +2,16 @@
  * @Author: Wanko
  * @Date: 2023-05-08 19:32:34
  * @LastEditors: Wanko
- * @LastEditTime: 2023-05-19 10:55:38
+ * @LastEditTime: 2023-05-19 11:20:44
  * @Description: 
  */
-import resolve from '@rollup/plugin-node-resolve'
-
 import terser from '@rollup/plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/caring-request/index.js',
   output: [
     {
-      file: 'src/dist/index.js'
+      file: 'dist/index.js'
     }
   ],
   plugins: [terser()]
--- src/request/api.js
+++ src/request/api.js
@@ -2,7 +2,7 @@
  * @Author: Wanko
  * @Date: 2023-05-18 15:49:47
  * @LastEditors: Wanko
- * @LastEditTime: 2023-05-19 11:02:49
+ * @LastEditTime: 2023-05-19 11:22:54
  * @Description: 
  */
 import request from "../caring-request"
